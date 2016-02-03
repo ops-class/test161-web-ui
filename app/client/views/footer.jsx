25f44b18c6987dd42eefc356a76f23b82aa09a32
xcv58 <chenyihonglove@gmail.com>
2016-02-03 11:16:54 -0500

    update Meteor app, get navigation.js, footer.js, head.html from www submodule

---
+++ app/client/views/footer.jsx
@@ -0,0 +1,18 @@
+FooterComponent = React.createClass({
+  render: function() {
+    return (
+
+      <div className="container">
+        <div className="row" style={{marginTop: 10, marginBottom: 20}}>
+          <div className="col-xs-12">
+            <hr />
+          </div>
+          <div className="small col-xs-12 col-md-4 col-md-offset-4 text-center">
+            Created 2/3/2016
+            <br /> Updated 2/3/2016
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
--- app/client/views/navigation.jsx
+++ app/client/views/navigation.jsx
@@ -1,55 +1,45 @@
 NavigationComponent = React.createClass({
-  mixins: [ReactMeteorData],
-  getMeteorData() {
-    return subscribeUserData();
-  },
-  render() {
-    if (!this.data.ready) {
-      return (<LoadingComponent />);
-    }
-    const asstLinks = [
-      {name: "ASST0", href: "/asst/0"}
-      // ,{name: "ASST1", href: "/asst/1"}
-      // ,{name: "ASST2", href: "/asst/2"}
-      // ,{name: "ASST3", href: "/asst/3"}
-    ].map((link) => {
-      return (<AsstLinkComponent key={link.name} link={link}/>);
-    });
-
-    const LoginoutButton = (<LoginOutComponent user={this.data.user}/>);
+  render: function() {
     return (
+
       <div className="container">
         <nav className="navbar navbar-default navbar-fixed-top">
-          <a className="logo-fixed" href="/"><img src="/img/logos/ops-class.jpg" alt="ops-class.org logo"/></a>
+          <a className="logo-fixed" href="/"><img src="/img/logos/ops-class.jpg" alt="ops-class.org logo" /></a>
           <div className="container">
             <div className="navbar-header">
               <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                 <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
               </button>
             </div>
             <div id="navbar" className="navbar-collapse collapse">
               <div className="row">
                 <div className="col-sm-6">
+                  {/*
+								<ul class="nav navbar-nav left">
+									<li><a id="menu-lectures"
+											title="Lecture slides, notes, and videos."
+											href="/lectures/">lectures</a>
+									</li>
+									<li><a id="menu-courses"
+											title="Exams to test your knowledge of OS concepts."
+											href="/exams/">exams</a></li>
+									<li><a title="Links to information about specific courses using ops-class.org."
+											href="/courses/">courses</a></li>
+								</ul>
+								*/}
                 </div>
                 <div className="col-sm-6">
                   <ul className="nav navbar-nav right">
                     <li className="dropdown">
-                      <a href="#" className="dropdown-toggle" id="menu-asst"
-                         title="Hack the kernel! OS/161-based operating system assignments."
-                         data-toggle="dropdown" role="button"
-                         aria-haspopup="true" aria-expanded="false">assignments</a>
+                      <a href="#" className="dropdown-toggle" id="menu-asst" title="Hack the kernel! OS/161-based operating system assignments." data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">assignments</a>
                       <ul className="dropdown-menu">
-                        {asstLinks}
+                        <li><a href="/asst/0/">ASST0</a></li>
                       </ul>
                     </li>
-                    <li><a id="menu-discuss"
-                           title="Discourse-based course discussion forum."
-                           target="_blank"
-                           href="https://discourse.ops-class.org">discuss</a></li>
-                    {LoginoutButton}
+                    <li><a id="menu-discuss" title="Discourse-based course discussion forum." target="_blank" href="https://discourse.ops-class.org" className="external">discuss</a></li>
                   </ul>
                 </div>
               </div>
@@ -57,29 +47,6 @@ NavigationComponent = React.createClass({
           </div>
         </nav>
       </div>
-    );
-  }
-});
-
-AsstLinkComponent = React.createClass({
-  render() {
-    const {name, href} = this.props.link;
-    return (
-      <li><a href={href}>{name}</a></li>
-    );
-  }
-});
-
-LoginOutComponent = React.createClass({
-  render() {
-    let [text, callback] = ["Login", login];
-    if (this.props.user) {
-      [text, callback] = ["Logout", logout];
-    }
-    return (
-      <li onClick={callback}>
-        <a href="#" title={text}>{text}</a>
-      </li>
     );
   }
 });
