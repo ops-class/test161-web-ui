10f0b8b85a35263a8e68d3453a436ed13d4238c0
xcv58 <chenyihonglove@gmail.com>
2016-02-08 23:30:36 -0500

    add LoadingComponent, LoginOutComponent

--- app/client/views/main_layout.jsx
+++ app/client/views/main_layout.jsx
@@ -1,15 +1,44 @@
 MainLayout = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData() {
+    return subscribeUserData();
+  },
   render() {
+    const content = this.data.ready ? this.props.content : <LoadingComponent />;
+    const {user} = this.data;
     return (
       <div>
         <NavigationComponent />
         <div id="content">
           <div className="container">
-            {this.props.content}
+            <LoginOutComponent user={user} />
+          </div>
+          <div className="container">
+            {content}
           </div>
         </div>
         <FooterComponent />
       </div>
     );
+  }
+});
+
+LoadingComponent = () => {
+  return (
+    <div>Loading</div>
+  );
+};
+
+LoginOutComponent = React.createClass({
+  render() {
+    if (this.props.user) {
+      return (
+        <button onClick={logout} className="btn btn-lg btn-danger">Logout</button>
+      );
+    } else {
+      return (
+        <button onClick={login} className="btn btn-lg btn-primary">Login</button>
+      );
+    }
   }
 });
