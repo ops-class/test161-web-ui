d3e3d6bf2c437c1dd4476c57651564c1b6be6dfd
xcv58 <chenyihonglove@gmail.com>
2016-02-12 09:35:27 -0500

    Move debug code to server/debug, add ensureindex.js

---
+++ app/server/debug/inititems.js
@@ -0,0 +1,136 @@
+generateOutput = (i = 0) => {
+  const line = 'line ' + i;
+  const walltime = 0.1 * i;
+  const simtime = walltime;
+  return {line, walltime, simtime};
+}
+
+generateCommand = () => {
+  const _id = Meteor.uuid();
+  const points_avail = 10;
+  const points_earned = 10;
+  const status = commandStatus[0];
+  const output = [];
+  return {_id, points_avail, points_earned, output, status};
+}
+
+generateTest = (index = -1, target = '', num = 2) => {
+  const _id = Meteor.uuid();
+  const name = `${target} test ${index}`;
+  const commands = [];
+  const points_avail = randomInt(10);
+  const points_earned = randomInt(points_avail);
+  const status = testStatus[0];
+  const test = {_id, name, commands, points_avail, points_earned, status};
+  Tests.insert(test);
+  for (let i = 0; i < num; i++) {
+    Meteor.setTimeout(() => {
+      const command = generateCommand();
+      commands.push(command);
+      Tests.update(_id, { $set: { commands: commands } });
+      for (let j = 0; j < 10; j++) {
+        Meteor.setTimeout(() => {
+          command.status = commandStatus[1];
+          command.output.push(generateOutput(j));
+          if (command.output.length === 10) {
+            command.status = commandStatus[2];
+          }
+          Tests.update(_id, { $set: {commands: commands } });
+          if (command.output.length === 10 && commands.length == num) {
+            Tests.update(_id, { $set: {status: testStatus[1] } });
+          }
+        }, 1000 * j);
+      }
+    }, 1000 * 10 * i);
+  }
+  return test;
+}
+
+generateMockTest = ({_id}, num = 2) => {
+  const submission = Submissions.findOne(_id);
+  if (!submission) {
+    console.log('generateMockTest:', _id, 'not found in Submissions!');
+    return;
+  }
+  const {tests} = submission;
+  if (tests.length !== 0) {
+    console.log('generateMockTest:', `tests of ${_id} is not empty`);
+    return;
+  }
+  for (let i = 0; i < num; i++) {
+    Meteor.setTimeout(() => {
+      const test = generateTest(i, _id);
+      tests.push(test._id);
+      Submissions.update(_id, { $set : { tests: tests } });
+    }, 1000 * 10 * num * i);
+  }
+}
+
+getRandomSubmission = (index = -1) => {
+  if (index === -1 || index >= submissionStatus.length) {
+    index = Math.floor(Math.random() * submissionStatus.length);
+  }
+  const _id = Meteor.uuid();
+  const randomTime = moment().subtract(Math.floor(Math.random() * 160000), 'seconds');
+  const submission_time = randomTime.toDate();
+  const users = ['t1@xcv58.com', 't2@xcv58.com'];
+  const repository = 'git@github.com:ops-class/test161.git';
+  const commit_id = '9352d6f4ad60d5183b678d64b9964f8c2a58c0db';
+  const status = submissionStatus[index];
+  const tests = [];
+  const target = _id;
+  const target_name = targetNames[Math.floor(Math.random() * targetNames.length)];
+  const max_score = 100;
+  const completion_time = moment(randomTime).add(Math.floor(Math.random() * 10000), 'seconds').toDate();
+  if (isSubmitted(status)) {
+    return {_id, submission_time, users, repository, commit_id, status, target, max_score, target_name, tests};
+  } else if (isFailed(status)) {
+    return {_id, submission_time, users, repository, commit_id, status, target, max_score, target_name, tests, completion_time};
+  } else {
+    const score = Math.floor(Math.random() * 100);
+    return {_id, submission_time, users, repository, commit_id, status, target, max_score, target_name, tests, completion_time, score};
+  }
+}
+
+initSubmissions = (count = 10) => {
+  for (let i = 0; i < count; i++) {
+    const sub = getRandomSubmission(i);
+    Submissions.insert(sub);
+  }
+}
+
+clean = () => {
+  Tests.remove({});
+  Submissions.remove({});
+}
+
+mock = (score = 0, num = 2) => {
+  const submission = Submissions.findOne({score}, {sort: { submission_time: -1 }});
+  if (!submission) {
+    console.log('No submission with score: ' + score);
+    return;
+  }
+  generateMockTest(submission, num);
+}
+
+initItems = (num = 1000) => {
+  const count = Submissions.find().count();
+  if (count < num) {
+    initSubmissions(num - count);
+  }
+}
+
+Meteor.methods({
+  initItems: (num) => {
+    initItems(num);
+    return num;
+  },
+  mock: (score = 100, num = 2) => {
+    mock(score, num);
+    return `mock for score ${score} with ${num} tests`;
+  },
+  clean: () => {
+    clean();
+    return 'clean';
+  }
+});
---
+++ app/server/ensureindex.js
@@ -0,0 +1,1 @@
+Submissions._ensureIndex({ "users": 1, "target_name": 1, "submission_time": 1 });
