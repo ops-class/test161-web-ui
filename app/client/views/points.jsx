3ff84d0b4aa155c97338be0ba8c1e8b154bf9802
xcv58 <chenyihonglove@gmail.com>
2016-02-14 20:46:49 -0500

    add UI for summary. use alert instead of label

--- app/client/views/points.jsx
+++ app/client/views/points.jsx
@@ -1,20 +1,20 @@
 getPointClass = ({points_earned, points_avail}) => {
   const ratio = points_avail ? points_earned / points_avail : 0;
   if (ratio < 0.3) {
-    return 'label-important';
+    return 'danger';
   }
   if (ratio < 0.6) {
-    return 'label-warning';
+    return 'warning';
   }
   if (ratio < 0.9) {
-    return 'label-info';
+    return 'info';
   }
-  return 'label-success';
+  return 'success';
 }
 
 PointComponent = ({points_earned, points_avail}) => {
-  let className = 'label ';
-  className += getPointClass({points_earned, points_avail});
+  let className = 'alert ';
+  className += 'alert-' + getPointClass({points_earned, points_avail});
   console.log(className);
   return (
     <span className={className}>
--- app/client/views/summary.jsx
+++ app/client/views/summary.jsx
@@ -2,10 +2,35 @@ SummaryComponent = React.createClass({
   render() {
     console.log(this.props);
     const {student} = this.props;
-    const {email } = student;
+    const {email} = student;
+    const scores = [
+      {asst: 'asst1', points_earned: 1, points_avail: 10},
+      {asst: 'asst2', points_earned: 5, points_avail: 10},
+      {asst: 'asst3', points_earned: 10, points_avail: 10}
+    ]
+    const list = scores.map(score => {
+      const {points_earned, points_avail, asst} = score;
+      const className = getPointClass(score);
+      return (
+        <tr key={asst} className={className}>
+          <td>{asst}</td>
+          <td>{points_earned}/{points_avail}</td>
+        </tr>
+      );
+    });
     return (
       <div className="row">
-        {email}
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Assignment</th>
+              <th>Best Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list}
+          </tbody>
+        </table>
       </div>
     );
   }
