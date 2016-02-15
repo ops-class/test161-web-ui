606b3882b4efffd63fca412714692eaea520818b
xcv58 <chenyihonglove@gmail.com>
2016-02-14 23:04:54 -0500

    show student.target_results information on summary section

--- app/client/views/summary.jsx
+++ app/client/views/summary.jsx
@@ -1,13 +1,18 @@
 SummaryComponent = React.createClass({
   render() {
-    console.log(this.props);
     const {student} = this.props;
-    const {email} = student;
-    const scores = [
-      {asst: 'asst1', points_earned: 1, points_avail: 10},
-      {asst: 'asst2', points_earned: 5, points_avail: 10},
-      {asst: 'asst3', points_earned: 10, points_avail: 10}
-    ]
+    const {email, target_results} = student;
+    let scores = [];
+    for (let asst of Object.keys(target_results)) {
+      const {
+        score: points_earned,
+        score: points_avail,
+        status,
+        performance,
+        submission_id
+      } = target_results[asst];
+      scores.push({asst, points_avail, points_earned});
+    }
     const list = scores.map(score => {
       const {points_earned, points_avail, asst} = score;
       const className = getPointClass(score);
