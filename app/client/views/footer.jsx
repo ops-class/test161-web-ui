affce5e4be6e4bd27f775a330379787fbd050daa
xcv58 <chenyihonglove@gmail.com>
2016-02-19 18:07:15 -0500

    pull update from www

--- app/client/views/footer.jsx
+++ app/client/views/footer.jsx
@@ -8,8 +8,12 @@ FooterComponent = React.createClass({
             <hr />
           </div>
           <div className="small col-xs-12 col-md-4 col-md-offset-4 text-center">
-            Created 2/3/2016 <br />
-            Updated 2/7/2016
+            Created 2/18/2016 <br />
+            Updated 2/19/2016
+          </div>
+          <div className="small col-xs-12 col-md-4 text-center">
+            <a href="https://github.com/ops-class/www/commits/master/src/asst/test161.adoc" target="_blank" className="external">File History</a> <br />
+            <a href="https://github.com/ops-class/www/blob/master/src/asst/test161.adoc" target="_blank" className="external">View on Github</a>
           </div>
         </div>
       </div>
