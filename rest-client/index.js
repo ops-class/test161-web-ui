d6d939db4e96a5f23b881deb2cf0b0ca9ed75830
xcv58 <chenyihonglove@gmail.com>
2016-03-16 17:19:08 -0400

    add rest-client to test api

---
+++ rest-client/index.js
@@ -0,0 +1,39 @@
+import {Client} from 'node-rest-client';
+
+const TOKEN = '3CCDbiRaopQKDmKqs';
+const ENDPOINT = 'http://127.0.0.1:3000/api-v1/scores/';
+
+const client = new Client();
+const data = {
+  target: "asst1",
+  deadline: new Date(),
+  users: [
+    {
+      deadline: '2016-03-16T03:39:26.894Z',
+      email: 'admin@ops-class.org',
+    },
+    {
+      email: 'Qon4n@buffalo.edu'
+    }
+  ]
+}
+
+
+const post = () => {
+  client.post(ENDPOINT,
+    {
+      data,
+      headers: {
+        "Content-Type": "application/json",
+        "X-Auth-Token": TOKEN
+      }
+    }, (data, response) => {
+      const {statusCode, statusMessage} = response;
+      console.log({statusCode, statusMessage});
+      console.log('----------------------------------------------------------------');
+      console.log(data);
+    }
+  )
+}
+
+setTimeout(post, 1);
