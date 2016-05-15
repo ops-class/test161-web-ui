83ab54f9215ffa9a29e8246c775a397fa28589ab
xcv58 <chenyihonglove@gmail.com>
2016-05-15 10:06:04 -0400

    update rest client

--- rest-client/index.js
+++ rest-client/index.js
@@ -1,29 +1,29 @@
 import {Scores, Hiddens} from './lib';
 
-const TOKEN = '3CCDbiRaopQKDmKqs';
+const TOKEN = 'xxxxxxxxxxxxxxxxx';
 const BASE_URL = 'http://127.0.0.1:3000/api-v2';
 
-// Scores.post(
-//   BASE_URL, {
-//     target: "asst1",
-//     deadline: new Date(),
-//     users: [
-//       {deadline: '2016-03-16T03:39:26.894Z', email: 'admin@ops-class.org'},
-//       {email: 'Qon4n@buffalo.edu'}
-//     ]
-//   },
-//   TOKEN
-// );
-
-Hiddens.get(BASE_URL, 'asst1', TOKEN);
-
-Hiddens.post(
-  BASE_URL,
-  {
-    target: 'asst1',
+Scores.post(
+  BASE_URL, {
+    target: 'asst3',
+    version: 1,
+    deadline: new Date(),
     users: [
-      {email: 'admin@ops-class.org'}
+      {email: 'admin@buffalo.edu'}
     ]
   },
   TOKEN
 );
+
+// Hiddens.get(BASE_URL, 'asst1', TOKEN);
+//
+// Hiddens.post(
+//   BASE_URL,
+//   {
+//     target: 'asst1',
+//     users: [
+//       {email: 'admin@ops-class.org'}
+//     ]
+//   },
+//   TOKEN
+// );
